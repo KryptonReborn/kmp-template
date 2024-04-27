7549eefc8009b966e2c88e1d85fe400342f11c04
14H <do.duc.hieu@linecorp.com>
2024-04-27 10:19:14 +0700

    Add publish-packages.yml & build.gradle.kts for publish

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,15 +1,43 @@
 plugins {
     id(libs.plugins.commonMppLib.get().pluginId)
+    id("maven-publish")
 }
+
+version = "0.0.1"
 
 android {
     namespace = "kmp.template"
 }
 
 kotlin {
+    androidTarget {
+        publishLibraryVariants("release", "debug")
+        publishLibraryVariantsGroupedByFlavor = true
+    }
     sourceSets {
         val commonMain by getting {
             dependencies {}
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/KryptonReborn/kmp-template")
+            credentials {
+                username =
+                    project.findProperty("gpr.user") as? String ?: System.getenv("GITHUB_USERNAME")
+                password =
+                    project.findProperty("gpr.key") as? String ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("multiplatform") {
+            groupId = "kmp.template"
+            artifactId = "library"
+            from(components["kotlin"])
         }
     }
 }
