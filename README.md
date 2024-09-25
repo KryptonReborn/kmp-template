8572561e7536581f35184cd0eec99396827b7376
14H <do.duc.hieu@linecorp.com>
2024-09-25 14:36:42 +0700

    Update template Move kover to common update action deprecated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![GitHub release](https://img.shields.io/badge/release-v0.0.2-blue.svg)](https://github.com/KryptonReborn/kmp-template/releases/tag/v0.0.2) [![Kotlin Version](https://img.shields.io/badge/Kotlin-1.9.23-B125EA?logo=kotlin)](https://kotlinlang.org)
 [![Build Status](https://github.com/saschpe/kase64/workflows/Main/badge.svg)](https://github.com/KryptonReborn/kmp-template/actions)
 [![License](http://img.shields.io/:License-Apache-blue.svg)](http://www.apache.org/licenses/LICENSE-2.0.html)
-![coverage badge](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/iTanChi/<gistId>/raw/<filename>.json)
+![coverage badge](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/iTanChi/80bcc643522fc574144cb35911894d21/raw/kotlin-template-coverage-badge.json)
 
 ![badge-android](http://img.shields.io/badge/Platform-Android-brightgreen.svg?logo=android)
 ![badge-ios](http://img.shields.io/badge/Platform-iOS-orange.svg?logo=apple)
--- build-logic/src/main/kotlin/extensions/Project.kt
+++ build-logic/src/main/kotlin/extensions/Project.kt
@@ -3,6 +3,44 @@ package extensions
 import org.gradle.api.Project
 import org.gradle.api.artifacts.*
 import org.gradle.kotlin.dsl.getByType
+import org.w3c.dom.Document
+import javax.xml.parsers.DocumentBuilderFactory
 
 val Project.libs
     get(): VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+fun Project.registerPrintLineCoverageTask() {
+    tasks.register("printLineCoverage") {
+        group = "verification" // Put into the same group as the `kover` tasks
+        dependsOn("koverXmlReport")
+        doLast {
+            val report = file("$projectDir/build/reports/kover/report.xml")
+
+            val doc: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
+            val rootNode = doc.firstChild
+            var childNode = rootNode.firstChild
+
+            var coveragePercent = 0.0
+
+            while (childNode != null) {
+                if (childNode.nodeName == "counter") {
+                    val typeAttr = childNode.attributes.getNamedItem("type")
+                    if (typeAttr.textContent == "LINE") {
+                        val missedAttr = childNode.attributes.getNamedItem("missed")
+                        val coveredAttr = childNode.attributes.getNamedItem("covered")
+
+                        val missed = missedAttr.textContent.toLong()
+                        val covered = coveredAttr.textContent.toLong()
+
+                        coveragePercent = (covered * 100.0) / (missed + covered)
+
+                        break
+                    }
+                }
+                childNode = childNode.nextSibling
+            }
+
+            println("%.1f".format(coveragePercent))
+        }
+    }
+}
